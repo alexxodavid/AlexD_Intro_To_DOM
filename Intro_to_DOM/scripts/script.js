eb21e22b2c3dafde4c2f5eff4ecd6e700379a619
Alex Odysseus David <alexxodavid@gmail.com>
2025-04-07 01:03:31 +0000

    querySelector() helps us select elements using CSS selectors; making it easier to target specific elements in our HTML.

--- Intro_to_DOM/scripts/script.js
+++ Intro_to_DOM/scripts/script.js
@@ -1,3 +1,11 @@
-let titleElement = document.getElementById("title");
+// Select the h1 element
+let mainHeading = document.querySelector("#main-heading");
+console.log(mainHeading);
 
-console.log(titleElement);
+// Select the paragraph element
+let description = document.querySelector(".description");
+console.log(description);
+
+// Select the button element
+let changeTextButton = document.querySelector("#changingText");
+console.log(changeTextButton);
