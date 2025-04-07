6e61515cb40ad069eabb254924a364abdd9aa71f
Alex Odysseus David <alexxodavid@gmail.com>
2025-04-07 01:07:19 +0000

    querySelectorAll() allows us to select multiple elements at once.

--- Intro_to_DOM/scripts/script.js
+++ Intro_to_DOM/scripts/script.js
@@ -1,11 +1,2 @@
-// Select the h1 element
-let mainHeading = document.querySelector("#main-heading");
-console.log(mainHeading);
-
-// Select the paragraph element
-let description = document.querySelector(".description");
-console.log(description);
-
-// Select the button element
-let changeTextButton = document.querySelector("#changingText");
-console.log(changeTextButton);
+let descriptions = document.querySelectorAll("p");
+console.log(descriptions);
